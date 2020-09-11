66050050b714de84cd49dcd1a3b725ed832f2e54
darshan <darshanaswath@gmail.com>
2020-09-11 11:51:19 +0530

    new version

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ heroku run python manage.py flush --noinput
 git add .
 git commit -m "new version"
 git push heroku master
-#curl https://apitmindia.herokuapp.com/tran/latest
+# curl https://apitmindia.herokuapp.com/tran/latest
