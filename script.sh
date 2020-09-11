c09358cbcf1e6c6d0518dd12211cd85331ec65d7
darshan <darshanaswath@gmail.com>
2020-09-11 21:56:54 +0530

    backtosql

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 python manage.py flush --noinput
-curl https://apitmindia.herokuapp.com/tran/latest
+# curl https://apitmindia.herokuapp.com/tran/latest
+
+
+# TRUNCATE TABLE transfermktAPI_transfer CASCADE;
