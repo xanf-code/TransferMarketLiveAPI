3aacc3fcf6c27c4061ed8a8047df668229cea4ec
darshan <darshanaswath@gmail.com>
2020-09-12 01:33:10 +0530

    ok

--- transfermktAPI/management/commands/script.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand
-from transfermktAPI.models import Transfer
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        Transfer.objects.all().delete()
