b226429f681b68f781ead79de93b89fa3615c8d8
darshan <darshanaswath@gmail.com>
2020-09-12 10:05:36 +0530

    mongoMigration

--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # python manage.py flush --noinput
-curl https://apitmindia.herokuapp.com/tran/latest
-
-
+# curl https://apitmindia.herokuapp.com/tran/latest
 # TRUNCATE TABLE transfermktAPI_transfer CASCADE;
