75fbb803385fcae0346376dd0030d50941badb55
darshan <darshanaswath@gmail.com>
2020-09-13 20:02:51 +0530

    QUERY

---
+++ DeleteQuery.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    database = "dd75re8gpemlhh",
+    user = "dgasqbsxpyknsj",
+    password = "b291aec1072c11c216a18636825ae58778688c3d57ee779cd951ddbb9299e6ce",
+    host = "ec2-18-210-51-239.compute-1.amazonaws.com",
+    port = "5432"
+)
+
+cur = conn.cursor()
+
+cur.execute("DELETE from transfermktAPI_transfer where id =1")
+
+conn.comit()
+
+print("QUERY EXECUTED")
+
+conn.close()
