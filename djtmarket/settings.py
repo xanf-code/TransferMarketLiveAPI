a04d009defeb3d196c51c608ddfaa98e34bc9fa4
darshan <darshanaswath@gmail.com>
2020-09-14 00:48:07 +0530

    reactIntegration

--- djtmarket/settings.py
+++ djtmarket/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '22x#@v(&lm@d9k8dhmys1tdyx0y41r@x!)gd#1(2%$ua%+hqvs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0','localhost','apitmindia.herokuapp.com','127.0.0.1']
 
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'staticfiles', 'reactapp/src')
+            os.path.join(BASE_DIR, 'reactapp/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -159,4 +159,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'reactapp/src')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles' , 'reactapp/build/static')
