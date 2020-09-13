9295943c0642609cba664ea5e99dae92641f7885
darshan <darshanaswath@gmail.com>
2020-09-14 00:51:24 +0530

    reactIntegration

--- djtmarket/settings.py
+++ djtmarket/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'staticfiles','reactapp/build')
+            os.path.join(BASE_DIR, 'staticfiles','reactapp/build/static')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
