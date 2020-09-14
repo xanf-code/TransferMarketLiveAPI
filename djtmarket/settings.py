3719f1d70d4ab373d4c9a0309c29ef42de99c325
darshan <darshanaswath@gmail.com>
2020-09-14 21:18:08 +0530

    app

--- djtmarket/settings.py
+++ djtmarket/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'reactapp/build')
+            os.path.join(BASE_DIR, 'frontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
